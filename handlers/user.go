70e18d533d1b19651ff00f8bce62dc733bfd7b04
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-03-27 19:20:07 -0300

    code ok

--- handlers/user.go
+++ handlers/user.go
@@ -1,5 +1,5 @@
 package handlers
 
-func Signup() {
+// func Signup() {
 
-}
+// }
--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	router.Use(gin.Logger())
 	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
-	router.GET("/api-1", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"sucess": "Access granted for api-1"})
-	})
 	router.GET("/api-2", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"sucess": "Access granted for api-2"})
+	})
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"sucess": "Access granted for api-1"})
 	})
 	router.Run(":8080")
 }
