bfb7e23edc5950c87bddbc6e7d7960b356e56a18
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-03-27 22:51:30 -0300

    Routes Update

--- main.go
+++ main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"golang-jwt/routes"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.New()
-	router.Use(gin.Logger())
-	routes.AuthRoutes(router)
-	routes.UserRoutes(router)
-	router.GET("/api-2", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"sucess": "Access granted for api-2"})
-	})
-	router.GET("/api-1", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"sucess": "Access granted for api-1"})
-	})
-	router.Run(":8080")
+	// Setup Gin-Gonic
+	routes.RoutesSetup()
 }
